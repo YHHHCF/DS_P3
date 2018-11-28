39959b004115143c8844b02c115aa50c66872b96
gauss0818 <siyugao0818@gmail.com>
2018-11-27 21:45:00 -0500

    gsy_1127

--- src/github.com/cmu440/tribbler/storageserver/storageserver_impl.go
+++ src/github.com/cmu440/tribbler/storageserver/storageserver_impl.go
@@ -4,45 +4,37 @@ import (
 	"fmt"
 	"github.com/cmu440/tribbler/libstore"
 	"github.com/cmu440/tribbler/rpc/storagerpc"
-	"log"
 	"net"
 	"net/http"
 	"net/rpc"
-	"os"
 	"sync"
 	"time"
 )
 
-var LOGE = log.New(os.Stderr, "", log.Lshortfile|log.Lmicroseconds)
-
 type storageServer struct {
-	//masterServerHostPort string
 	hostPort string
 
-	isMaster   bool
-	nodes      []storagerpc.Node
-	numNodes   int
-	virtualIDs []uint32
+	isMaster   bool              // whether the storage server is master server
+	nodes      []storagerpc.Node // all the storage servers
+	numNodes   int               // number of all the servers on the ring
+	virtualIDs []uint32          // virtualIDs for this storage server
 
-	listMap     map[string][]string
-	itemMap     map[string]string
-	revokingMap map[string]string
-	leaseMap    map[string]map[string]*leaseContent
+	listMap     map[string][]string                 // map for list storing client and tribbler ID
+	itemMap     map[string]string                   // map for item storing tribblers
+	revokingMap map[string]string                   // map for revoking lease request
+	leaseMap    map[string]map[string]*leaseContent // map a key to leases (different hostports)
 
-	sServerLock     *sync.Mutex // protect the R/W of server's content
+	sServerLock     *sync.Mutex
 	listMapLock     *sync.Mutex
 	itemMapLock     *sync.Mutex
 	revokingMapLock *sync.Mutex
 	revokingMapCond *sync.Cond
 	leaseMapLock    *sync.Mutex
-
-	client *rpc.Client // the connection to the master
-
 }
 
 type leaseContent struct {
 	expTime  time.Time
-	hostport string
+	hostPort string
 }
 
 // NewStorageServer creates and starts a new StorageServer. masterServerHostPort
@@ -54,22 +46,21 @@ type leaseContent struct {
 // This function should return only once all storage servers have joined the ring,
 // and should return a non-nil error if the storage server could not be started.
 func NewStorageServer(masterServerHostPort string, numNodes, port int, virtualIDs []uint32) (StorageServer, error) {
-	//fmt.Println(masterServerHostPort, numNodes, port, virtualIDs)
-	sServer := new(storageServer)
-	sServer.nodes = []storagerpc.Node{}
-	sServer.virtualIDs = virtualIDs
-	sServer.numNodes = numNodes
-	sServer.itemMap = make(map[string]string)
-	sServer.listMap = make(map[string][]string)
-	sServer.revokingMap = make(map[string]string)
-	sServer.leaseMap = make(map[string]map[string]*leaseContent)
-	sServer.hostPort = fmt.Sprintf("localhost:%d", port)
-	sServer.sServerLock = new(sync.Mutex)
-	sServer.itemMapLock = new(sync.Mutex)
-	sServer.listMapLock = new(sync.Mutex)
-	sServer.revokingMapLock = new(sync.Mutex)
+	sServer := &storageServer{
+		nodes:           []storagerpc.Node{},
+		virtualIDs:      virtualIDs,
+		numNodes:        numNodes,
+		itemMap:         make(map[string]string),
+		listMap:         make(map[string][]string),
+		revokingMap:     make(map[string]string),
+		leaseMap:        make(map[string]map[string]*leaseContent),
+		hostPort:        fmt.Sprintf("localhost:%d", port),
+		sServerLock:     new(sync.Mutex),
+		itemMapLock:     new(sync.Mutex),
+		listMapLock:     new(sync.Mutex),
+		revokingMapLock: new(sync.Mutex),
+		leaseMapLock:    new(sync.Mutex)}
 	sServer.revokingMapCond = sync.NewCond(sServer.revokingMapLock)
-	sServer.leaseMapLock = new(sync.Mutex)
 
 	// Create the server socket that will listen for incoming RPCs.
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
@@ -98,6 +89,7 @@ func NewStorageServer(masterServerHostPort string, numNodes, port int, virtualID
 	} else { // if this is slave server, call registerServer through rpc
 		sServer.isMaster = false
 		client, err := rpc.DialHTTP("tcp", masterServerHostPort)
+		// Keep trying to connect with masterServer
 		for {
 			if err == nil {
 				break
@@ -106,12 +98,14 @@ func NewStorageServer(masterServerHostPort string, numNodes, port int, virtualID
 			client, err = rpc.DialHTTP("tcp", masterServerHostPort)
 		}
 
-		sServer.client = client
-		var registerStatus = storagerpc.NotReady
-		for registerStatus != storagerpc.OK {
-			time.Sleep(time.Second)
+		// Wait until all the storagesevers are registered
+		client.Call("StorageServer.RegisterServer", registerArgs, registerReply)
+		for {
+			if registerReply.Status == storagerpc.OK {
+				break
+			}
+			time.Sleep(time.Duration(1000 * time.Millisecond))
 			client.Call("StorageServer.RegisterServer", registerArgs, registerReply)
-			registerStatus = registerReply.Status
 		}
 		sServer.nodes = registerReply.Servers
 	}
@@ -149,21 +143,19 @@ func (ss *storageServer) RegisterServer(args *storagerpc.RegisterArgs, reply *st
 
 func (ss *storageServer) GetServers(args *storagerpc.GetServersArgs, reply *storagerpc.GetServersReply) error {
 	ss.sServerLock.Lock()
-	defer ss.sServerLock.Unlock()
 	// check whether all the servers are registered
-	if len(ss.nodes) != ss.numNodes {
+	if len(ss.nodes) == ss.numNodes {
+		reply.Servers = ss.nodes
+		reply.Status = storagerpc.OK
+	} else {
 		reply.Status = storagerpc.NotReady
-		return nil
+
 	}
-
-	reply.Servers = ss.nodes
-	reply.Status = storagerpc.OK
-
+	ss.sServerLock.Unlock()
 	return nil
 }
 
 func (ss *storageServer) Get(args *storagerpc.GetArgs, reply *storagerpc.GetReply) error {
-	//index := ss.GetNearestNodeIndex(args.Key)
 	// check whether item is in this server
 	if ss.CheckNode(args.Key) {
 		ss.itemMapLock.Lock()
@@ -178,7 +170,7 @@ func (ss *storageServer) Get(args *storagerpc.GetArgs, reply *storagerpc.GetRepl
 
 		//if key does not exist in the server, still grant a lease
 		if args.WantLease {
-			reply.Lease = *ss.GiveLease(args.Key, args.HostPort)
+			reply.Lease = *ss.GrantLease(args.Key, args.HostPort)
 		}
 	} else {
 		reply.Status = storagerpc.WrongServer
@@ -187,15 +179,20 @@ func (ss *storageServer) Get(args *storagerpc.GetArgs, reply *storagerpc.GetRepl
 }
 
 func (ss *storageServer) Delete(args *storagerpc.DeleteArgs, reply *storagerpc.DeleteReply) error {
-	//index := ss.GetNearestNodeIndex(args.Key)
+	// check whether item is in this server
 	if ss.CheckNode(args.Key) {
-		ss.SetRevoking(args.Key)
+		// Store the key which need to be revoked
+		// in case of there are multiple revoke request for the same key
+		ss.InsertRevoking(args.Key)
+		// as long as leases are ready to be revoked
 		ss.RevokeLease(args.Key)
-		defer ss.ClearRevoking(args.Key)
+		// after fininshing revoking leases, delete the key in revokingMap
+		defer ss.DeleteRevoking(args.Key)
 
 		ss.itemMapLock.Lock()
 		defer ss.itemMapLock.Unlock()
 
+		// delete item if itemMap has key
 		if _, haskey := ss.itemMap[args.Key]; haskey {
 			delete(ss.itemMap, args.Key)
 			reply.Status = storagerpc.OK
@@ -209,7 +206,6 @@ func (ss *storageServer) Delete(args *storagerpc.DeleteArgs, reply *storagerpc.D
 }
 
 func (ss *storageServer) GetList(args *storagerpc.GetArgs, reply *storagerpc.GetListReply) error {
-	//index := ss.GetNearestNodeIndex(args.Key)
 	if ss.CheckNode(args.Key) {
 		ss.listMapLock.Lock()
 		if list, haskey := ss.listMap[args.Key]; haskey {
@@ -222,7 +218,7 @@ func (ss *storageServer) GetList(args *storagerpc.GetArgs, reply *storagerpc.Get
 		ss.listMapLock.Unlock()
 
 		if args.WantLease {
-			reply.Lease = *ss.GiveLease(args.Key, args.HostPort)
+			reply.Lease = *ss.GrantLease(args.Key, args.HostPort)
 		}
 	} else {
 		reply.Status = storagerpc.WrongServer
@@ -231,15 +227,19 @@ func (ss *storageServer) GetList(args *storagerpc.GetArgs, reply *storagerpc.Get
 }
 
 func (ss *storageServer) Put(args *storagerpc.PutArgs, reply *storagerpc.PutReply) error {
-	//index := ss.GetNearestNodeIndex(args.Key)
 	if ss.CheckNode(args.Key) {
-		ss.SetRevoking(args.Key)
+		// Store the key which need to be revoked
+		// in case of there are multiple revoke request for the same key
+		ss.InsertRevoking(args.Key)
+		// as long as leases are ready to be revoked
 		ss.RevokeLease(args.Key)
-		defer ss.ClearRevoking(args.Key)
+
+		defer ss.DeleteRevoking(args.Key)
 
 		ss.itemMapLock.Lock()
 		defer ss.itemMapLock.Unlock()
 
+		// insert tribbler into itemMap
 		ss.itemMap[args.Key] = args.Value
 		reply.Status = storagerpc.OK
 	} else {
@@ -250,34 +250,33 @@ func (ss *storageServer) Put(args *storagerpc.PutArgs, reply *storagerpc.PutRepl
 
 func (ss *storageServer) AppendToList(args *storagerpc.PutArgs, reply *storagerpc.PutReply) error {
 	if ss.CheckNode(args.Key) {
-		ss.SetRevoking(args.Key)
+		// Store the key which need to be revoked
+		// in case of there are multiple revoke request for the same key
+		ss.InsertRevoking(args.Key)
+		// as long as leases are ready to be revoked
 		ss.RevokeLease(args.Key)
-		defer ss.ClearRevoking(args.Key)
+		// after fininshing revoking leases, delete the key in revokingMap
+		defer ss.DeleteRevoking(args.Key)
 
 		ss.listMapLock.Lock()
 		defer ss.listMapLock.Unlock()
 
-		key := args.Key
-		value := args.Value
-
-		list, hasKey := ss.listMap[key]
+		list, hasKey := ss.listMap[args.Key]
 		if hasKey {
 			len := len(list)
-
 			i := 0
 			for i = 0; i < len; i++ {
-				if value == list[i] {
+				if args.Value == list[i] {
 					break
 				}
 			}
-
 			if i <= len-1 {
 				reply.Status = storagerpc.ItemExists
 				return nil
 			}
 		}
 
-		ss.listMap[key] = append(ss.listMap[key], value)
+		ss.listMap[args.Key] = append(ss.listMap[args.Key], args.Value)
 		reply.Status = storagerpc.OK
 	} else {
 		reply.Status = storagerpc.WrongServer
@@ -287,39 +286,43 @@ func (ss *storageServer) AppendToList(args *storagerpc.PutArgs, reply *storagerp
 
 func (ss *storageServer) RemoveFromList(args *storagerpc.PutArgs, reply *storagerpc.PutReply) error {
 	if ss.CheckNode(args.Key) {
-		ss.SetRevoking(args.Key)
+		// Store the key which need to be revoked
+		// in case of there are multiple revoke request for the same key
+		ss.InsertRevoking(args.Key)
+		// as long as leases are ready to be revoked
 		ss.RevokeLease(args.Key)
-		defer ss.ClearRevoking(args.Key)
+		// after fininshing revoking leases, delete the key in revokingMap
+		defer ss.DeleteRevoking(args.Key)
 
 		ss.listMapLock.Lock()
 		defer ss.listMapLock.Unlock()
 
-		key := args.Key
-		value := args.Value
-
-		list, hasKey := ss.listMap[key]
+		list, hasKey := ss.listMap[args.Key]
 		if !hasKey {
 			reply.Status = storagerpc.KeyNotFound
 			return nil
 		}
 
+		// delete value with key in the list
 		len := len(list)
 
 		i := 0
 		for i = 0; i < len; i++ {
-			if value == list[i] {
+			if args.Value == list[i] {
 				break
 			}
 		}
 
 		if len > 0 {
+			// when value with key is at the end of list
 			if i == len-1 {
-				ss.listMap[key] = list[:i]
+				ss.listMap[args.Key] = list[:i]
 			} else if i < len-1 {
+				//  when value with key is at the head of list
 				if i == 0 {
-					ss.listMap[key] = list[i+1:]
-				} else {
-					ss.listMap[key] = append(list[:i-1], list[i+1:]...)
+					ss.listMap[args.Key] = list[i+1:]
+				} else { // when value with key is in the middle of list
+					ss.listMap[args.Key] = append(list[:i-1], list[i+1:]...)
 				}
 			} else {
 				reply.Status = storagerpc.ItemNotFound
@@ -337,172 +340,132 @@ func (ss *storageServer) RemoveFromList(args *storagerpc.PutArgs, reply *storage
 	return nil
 }
 
-func (ss *storageServer) GiveLease(key string, hostport string) *storagerpc.Lease {
+func (ss *storageServer) GrantLease(key string, hostport string) *storagerpc.Lease {
 	ss.revokingMapLock.Lock()
+	result := new(storagerpc.Lease)
+	// lease with the same key is being revoked, cannot grant the lease
 	if _, haskey := ss.revokingMap[key]; haskey {
 		ss.revokingMapLock.Unlock()
-		return &storagerpc.Lease{
-			Granted:      false,
-			ValidSeconds: 0,
-		}
+		result.Granted = false
+		result.ValidSeconds = 0
 	} else {
 		ss.revokingMapLock.Unlock()
 		ss.leaseMapLock.Lock()
-		lease := new(leaseContent)
-		lease.hostport = hostport
-		lease.expTime = time.Now().Add(time.Duration((storagerpc.LeaseSeconds + storagerpc.LeaseGuardSeconds) * time.Second))
-
-		if v, haskey := ss.leaseMap[key]; !haskey {
+		lease := &leaseContent{
+			hostPort: hostport,
+			expTime:  time.Now().Add(time.Duration((storagerpc.LeaseSeconds + storagerpc.LeaseGuardSeconds) * time.Second))}
+		// first lease request for key, create leaseMap item
+		if leases, haskey := ss.leaseMap[key]; !haskey {
 			ss.leaseMap[key] = make(map[string]*leaseContent)
 			ss.leaseMap[key][hostport] = lease
-		} else {
-			v[hostport] = lease
+		} else { // otherwise, update the lease
+			leases[hostport] = lease
 		}
 		ss.leaseMapLock.Unlock()
-		return &storagerpc.Lease{
-			Granted:      true,
-			ValidSeconds: storagerpc.LeaseSeconds,
-		}
+		result.Granted = true
+		result.ValidSeconds = storagerpc.LeaseSeconds
 	}
+	return result
 }
 
-func (ss *storageServer) IsRevoking(key string) bool {
-	ss.revokingMapLock.Lock()
-	defer ss.revokingMapLock.Unlock()
-	if _, haskey := ss.revokingMap[key]; haskey {
-		return true
-	} else {
-		return false
-	}
-}
-
-func (ss *storageServer) SetRevoking(key string) {
+func (ss *storageServer) InsertRevoking(key string) {
 	ss.revokingMapLock.Lock()
 	_, haskey := ss.revokingMap[key]
+	// a revoking request is being delt with at the same time
+	// wait until the recent revoking request finish
 	for haskey == true {
 		ss.revokingMapCond.Wait()
 		_, haskey = ss.revokingMap[key]
 	}
-
+	// insert revoking request with key into revokingMap
 	ss.revokingMap[key] = ""
 	ss.revokingMapLock.Unlock()
 }
 
-func (ss *storageServer) ClearRevoking(key string) {
-
+func (ss *storageServer) DeleteRevoking(key string) {
 	ss.revokingMapLock.Lock()
 	defer ss.revokingMapLock.Unlock()
-
+	// finish this revoking action
 	delete(ss.revokingMap, key)
+	// notify the revoking request with the same key
 	ss.revokingMapCond.Broadcast()
 }
 
-//only revoke lease when write
+// only revoke lease when there is modification to the storage
 func (ss *storageServer) RevokeLease(key string) {
+	// check whether the leases with key are able to be revoked
 	ss.leaseMapLock.Lock()
 	leases, haskey := ss.leaseMap[key]
 	ss.leaseMapLock.Unlock()
-	if !haskey {
+	if haskey {
+		// Revoke leases with key
+		wg := new(sync.WaitGroup)
+		for hostport, lease := range leases {
+			if lease.expTime.After(time.Now()) {
+				wg.Add(1)
+				go ss.NotifyLibRevokeLease(key, lease, hostport, wg)
+			}
+		}
+		// wait until all the leases are revoked from libServer
+		wg.Wait()
+		delete(ss.leaseMap, key)
+	} else {
 		return
 	}
 
-	wg := new(sync.WaitGroup)
-
-	for hostport, lease := range leases {
-		if lease.expTime.After(time.Now()) {
-			wg.Add(1)
-			go func() {
-				status := "no"
-				for status == "no" {
-
-					libClient, err := rpc.DialHTTP("tcp", hostport)
-
-					if err != nil {
-						continue
-					}
-
-					revokeArgs := new(storagerpc.RevokeLeaseArgs)
-					revokeArgs.Key = key
-					revokeReply := new(storagerpc.RevokeLeaseReply)
-					revokeLease := libClient.Go("LeaseCallbacks.RevokeLease", revokeArgs, revokeReply, nil)
-					select {
-					case <-revokeLease.Done:
-						if revokeReply.Status == storagerpc.OK {
-							status = "ok"
-						}
-					case <-time.After(lease.expTime.Sub(time.Now())):
-						status = "ok"
-					}
-
-				}
-				wg.Done()
-			}()
-			//go ss.RevokeLeaseSubRoutine(key, hostport, lease)
-
-		}
-	}
-	wg.Wait()
-	delete(ss.leaseMap, key)
 }
 
-//func (ss *storageServer) RevokeLeaseSubRoutine(key string, hostport string, lease *leaseContent) {
-//	status := "no"
-//	for status == "no" {
-//
-//		libClient, err := rpc.DialHTTP("tcp", hostport)
-//
-//		if err != nil {
-//			continue
-//		}
-//
-//		revokeArgs := new(storagerpc.RevokeLeaseArgs)
-//		revokeArgs.Key = key
-//		revokeReply := new(storagerpc.RevokeLeaseReply)
-//		revokeLease := libClient.Go("LeaseCallbacks.RevokeLease", revokeArgs, revokeReply, nil)
-//		select {
-//		case <-revokeLease.Done:
-//			if revokeReply.Status == storagerpc.OK {
-//				status = "ok"
-//			}
-//		case <-time.After(lease.expTime.Sub(time.Now())):
-//			status = "ok"
-//		}
-//
-//	}
-//	wg.Done()
-//}
+func (ss *storageServer) NotifyLibRevokeLease(key string, lease *leaseContent, hostport string, wg *sync.WaitGroup) {
+	success := 0
+	for {
+		libClient, err := rpc.DialHTTP("tcp", hostport)
+		if err != nil {
+			continue
+		}
+		revokeArgs := new(storagerpc.RevokeLeaseArgs)
+		revokeReply := new(storagerpc.RevokeLeaseReply)
+		revokeArgs.Key = key
+		revokeLease := libClient.Go("LeaseCallbacks.RevokeLease", revokeArgs, revokeReply, nil)
+		select {
+		// libServer finish revoking lease
+		case <-revokeLease.Done:
+			if revokeReply.Status == storagerpc.OK {
+				success = 1
+			}
+		// lease expired
+		case <-time.After(lease.expTime.Sub(time.Now())):
+			success = 1
+		}
+		if success == 1 {
+			wg.Done()
+			return
+		}
+	}
+}
 
+// get the hostPort with the nearest vID which is equal or greater than the given ID
 func (ss *storageServer) GetNearestNodeIndex(key string) uint32 {
-	//args := &storagerpc.GetServersArgs{}
-	//var reply storagerpc.GetServersReply
-
-	//ss.client.Call("StorageServer.GetServers", args, &reply)
-
 	hash := libstore.StoreHash(key)
-	var next = ^uint32(0)
-	var nextIndex = -1
-	var min = ^uint32(0)
-	//var minIndex = -1
-	for i, node := range ss.nodes {
+	next := ^uint32(0)
+	min := ^uint32(0)
+	for _, node := range ss.nodes {
 		for _, id := range node.VirtualIDs {
 			if id >= hash && id < next {
 				next = id
-				nextIndex = i
 			}
 			if id < min {
 				min = id
 			}
 		}
 	}
-
-	if nextIndex != -1 {
+	if next != ^uint32(0) {
 		return next
 	} else {
 		return min
 	}
-
 }
 
+// check whether the virtulID suitable for key is in this server
 func (ss *storageServer) CheckNode(key string) bool {
 	ID := ss.GetNearestNodeIndex(key)
 	for _, value := range ss.virtualIDs {
